923dd2db1fa26bce388d69e4cc0829533e9ab44e
TanmayBhatt10 <bhatttanmay2110@gmail.com>
2026-09-20 13:20:11 +0530

    Add Section component and refactor Examples to use it

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import TabButton from './TabButton.jsx';
 import { EXAMPLES } from '../data.js';
+import Section from './Section.jsx';
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -25,7 +26,7 @@ export default function Examples() {
     );
     }
     return (
-        <section id="examples">
+        <Section id="examples">
             <h2>Examples</h2>
             <menu>
                 <TabButton
@@ -54,6 +55,6 @@ export default function Examples() {
                 </TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     );
 }
